75fb79488371aaf8d58c489b048b05ad6b77951f
baderex <baderex@users.noreply.github.com>
2025-06-24 11:30:51 +0000

    add index populate

--- chat_assist_driver.py
+++ chat_assist_driver.py
@@ -1,5 +1,7 @@
 import chat_assistant
 from openai import OpenAI
+from minsearch import Index, AppendableIndex
+import requests 
 
 client = OpenAI()
 
@@ -15,6 +17,25 @@ def search(query):
     )
 
     return results
+
+docs_url = 'https://github.com/alexeygrigorev/llm-rag-workshop/raw/main/notebooks/documents.json'
+docs_response = requests.get(docs_url)
+documents_raw = docs_response.json()
+documents = []
+
+for course in documents_raw:
+    course_name = course['course']
+
+    for doc in course['documents']:
+        doc['course'] = course_name
+        documents.append(doc)
+
+index = AppendableIndex(
+    text_fields=["question", "text", "section"],
+    keyword_fields=["course"]
+)
+
+index.fit(documents)
 
 search_description = {
     "type": "function",
